476fce9417645ef2b31cc5fbf91db5a0e8d7401f
thomscoder <moyelysian@icloud.com>
2022-08-13 12:17:11 +0200

    chore: added prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Grid from './components/Grid'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Grid from './components/Grid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <Grid size={40} cellSize={30}/>
+      <Grid size={40} cellSize={30} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Grid.tsx
+++ client/src/components/Grid.tsx
@@ -1,43 +1,47 @@
-import { useEffect, useState } from "react"
-import ShikariGrid, { ShGrid } from "../utils/grid"
+import { useEffect, useState } from 'react';
+import ShikariGrid, { ShGrid } from '../utils/grid';
 
 // css
-import "./Grid.css"
+import './Grid.css';
 
 export type GridProps = {
-	size: number;
-	cellSize: number;
+  size: number;
+  cellSize: number;
 };
 
-function Grid({size: GRID_SIZE, cellSize: CELL_SIZE}: GridProps): JSX.Element {
-	const [grid, setGrid] = useState<ShGrid>([])
-	
-	useEffect(() => {
-		const sGrid = new ShikariGrid(GRID_SIZE);
-		setGrid(sGrid.generateGrid());
-	}, [])
+function Grid({ size: GRID_SIZE, cellSize: CELL_SIZE }: GridProps): JSX.Element {
+  const [grid, setGrid] = useState<ShGrid>([]);
 
-	return (
-		<div className="grid">
-			{grid.map((row, rowIndex) => {
-				return (
-					<div key={rowIndex} className="row">
-							{row.map((cell, cellIndex) => {
-								console.log(cell);
-								return (
-									<div key={cellIndex} className={`cell ${cell}`} style={{
-										width: `${CELL_SIZE}px`,
-										height: `${CELL_SIZE}px`,
-									}}>
-										{cell}
-									</div>
-								)
-							})}
-					</div>
-				)
-			})}
-		</div>
-	)
+  useEffect(() => {
+    const sGrid = new ShikariGrid(GRID_SIZE);
+    setGrid(sGrid.generateGrid());
+  }, []);
+
+  return (
+    <div className="grid">
+      {grid.map((row, rowIndex) => {
+        return (
+          <div key={rowIndex} className="row">
+            {row.map((cell, cellIndex) => {
+              console.log(cell);
+              return (
+                <div
+                  key={cellIndex}
+                  className={`cell ${cell}`}
+                  style={{
+                    width: `${CELL_SIZE}px`,
+                    height: `${CELL_SIZE}px`,
+                  }}
+                >
+                  {cell}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Grid;
